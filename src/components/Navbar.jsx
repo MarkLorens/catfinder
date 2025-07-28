13aebe916a2d4995fe39aaea916c5f448addb270
MarkLorens <marklorens0705@gmail.com>
2025-07-28 12:33:34 +0700

    Scaffolding and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+    return (
+    <>
+        <div className="container">
+            <nav>
+                <ul>
+                    <li>Logo</li>
+                    <li>Keluhan</li>
+                </ul>
+            </nav>
+        </div>
+    </> );
+}
+
+export default Navbar;
